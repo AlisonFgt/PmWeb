7da4d7f9696dc14b5ed56b691d2eb3f84c2f3cdd
Desenv <Desenv@DESKTOP-S053JFJ>
2017-09-14 22:47:19 -0300

    Add project files.

---
+++ PmWeb.Core/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace PmWeb.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ PmWeb.Modelo/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace PmWeb
+{
+    public class Class1
+    {
+    }
+}
